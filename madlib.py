2f48fdd96748dd98653ef0eb7b1d6fe144b78f20
KALAGOTLARANGAREDDY <rangaredie@gmail.com>
2024-09-03 17:42:59 +0530

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,16 @@
+noun1 = "Massai"
+verb = "is"
+noun2 = "kenya"
+Adjective1 = "alarmed"
+Adjective2 = "educated"
+conjunction = "and"
+noun3 = "collaboration"
+noun4 = "Children"
+Adverb = "everywhere"
+
+Madlib =(f"Rebecca, The {noun1} Changemaker {verb} inspired by the true story of a young girl in {noun2} who was {Adjective1}\
+by the signs of climate change in her community. She {Adjective2} herself about the problem, then galvanized her \
+community {conjunction} the government to take action.This children’s picture book is the result of a {noun3} \
+between Ashoka and Worldreader. It is intended to inspire {noun4} {Adverb} to become Changemakers and encourag adults everywhere to support them in their journey.")
+print(Madlib)
+
